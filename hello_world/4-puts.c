49caf4beddd9bbf724c96ae8bd574412c9181ae2
neisnei <neischalyruidiaz@gmail.com>
2023-06-16 18:40:52 +0000

    hello,puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entery point from the main fuction oyu can call other functions
+ *
+ * Return:  Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
