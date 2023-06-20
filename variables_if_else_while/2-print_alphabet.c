fe44ffae2da34a7bed3bf60f21eff1092692837b
neisnei <neischalyruidiaz@gmail.com>
2023-06-20 18:10:23 +0000

    printsthatalphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * prints the alphabet in lowercase
+ *
+ * Return 0
+ */
+
+int main(void)
+
+{
+	char c;
+	for (c= 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
