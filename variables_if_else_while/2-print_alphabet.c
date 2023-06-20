d4a883709998b8d3f593580b06c357f1c02f4b7f
neisnei <neischalyruidiaz@gmail.com>
2023-06-20 18:43:36 +0000

    printsthatalphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 	char c;
-	
+
 	for (c= 'a'; c <= 'z'; c++)
-		   puthar(c)
-	putchar('\n')
+	putchar(c);
+	putchar('\n');
 
 	return (0);
 }
