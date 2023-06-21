12798fa54e16a77aaad044a7728643308bae9b89
neisnei <neischalyruidiaz@gmail.com>
2023-06-21 18:51:26 +0000

    smileinthemirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabetin reverse
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i > 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar ('\n');
+	return (0);
+}
