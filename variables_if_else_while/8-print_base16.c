4530867c0212bad8caaee9180bfa872dd1b92078
neisnei <neischalyruidiaz@gmail.com>
2023-06-21 19:56:34 +0000

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; i < 16; C++)
+	{
+		putchar ('0' + c);
+	}
+	return (0);
+}
