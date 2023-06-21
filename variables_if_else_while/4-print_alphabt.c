6a40a0989e938bf0646bbdc796b0c02ff3e6703e
neisnei <neischalyruidiaz@gmail.com>
2023-06-21 15:24:36 +0000

    alphabet_soup

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if ( c != 'q'; && c != 'e')
+			putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
