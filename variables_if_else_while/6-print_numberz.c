d2557d54d89d819903af97aeabc6425a036d3a00
neisnei <neischalyruidiaz@gmail.com>
2023-06-21 17:54:51 +0000

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all sinlge digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i
+		for (i = '0'; < i = '10'; i++)
+		{
+			putchar (i);
+		}
+	putchar ('\n)
+		return (0);
+}
