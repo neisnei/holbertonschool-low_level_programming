3b5f379c3ebf7e91b7d2ba6ab08a3aea948fed6b
neisnei <neischalyruidiaz@gmail.com>
2023-06-21 17:32:17 +0000

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("0123456789\n")
+		return (0);
+}
