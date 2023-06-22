d79689e1bf8334c459462d52e93296dc9f0f5674
neisnei <neischalyruidiaz@gmail.com>
2023-06-22 18:31:01 +0000

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code
+ * @c: letter
+ * Return: 1 if c is a letter
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		(c >= 'A' && c <= 'Z')
+			return (1);
+	else
+		return (0);
+}
