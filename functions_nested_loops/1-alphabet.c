c622d3962fd781aa81724ec43dc4e1d15d158773
neisnei <neischalyruidiaz@gmail.com>
2023-06-22 13:35:39 +0000

    printsthealphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	char c
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+	return (0);
+}
