7beb0acdffd3085a7b5e102ae200daea5f5a31d8
neisnei <neischalyruidiaz@gmail.com>
2023-06-23 13:24:02 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,23 +2,24 @@
 
 /**
  * print_sign - prints the sign of a number
- * Returns 1 and prints + if n is greater than zero
- * Returns 0 and prints 0 if n is zero
- * Returns -1 and prints - if n is less than zero
+ *
+ * @n: variable
+ *
+ * Returns -1 or 0
  *
  */
 
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if
+	if (n == 0)
 	{
 		_putchar('0');
 		return (0);
+	}
+	else if (n > 0);
+	{
+		_putchar('+');
+		return (1);
 	}
 	else
 	{
