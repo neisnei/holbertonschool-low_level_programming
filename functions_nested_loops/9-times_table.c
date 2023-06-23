b302ca4fd961b0fce6ce7113cfaf36a501c6ba21
neisnei <neischalyruidiaz@gmail.com>
2023-06-23 18:26:29 +0000

    learnyourtimestable

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,1 +1,50 @@
+#include "main.h"
 
+/**
+ * times_tablet - Prints the 9 times, starting with 0.
+ *
+ * Return: Always 0
+ */
+
+void times_table(void)
+	{
+	int x = 0, y, z;
+
+	if (n > 15 || n < 0)
+		return;
+	while (x <= n)
+	{
+		for (y = 0; y <= n; y++)
+		{
+			z = x * y;
+			if (z > 9)
+			{
+				_putchar(z / 9 + '0');
+				_putchar((z / 10 % 10) + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (z > 9)
+			{
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+				_putchar(z + '0');
+
+			if (y != n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		x++;
+	}
+}
