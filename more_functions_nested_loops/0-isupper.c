d1744030f069afb941ab4d5130a5bfb3b2127728
neisnei <neischalyruidiaz@gmail.com>
2023-06-24 01:47:13 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isupper - verifies if a character is uppercase or not
+*@c: variable
+*Return: returns 1 if it is uppercase , 0 if not
+*/
+
+int _isupper(int c)
+{
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+
+return (0);
+}
