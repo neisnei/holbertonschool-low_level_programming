13854d8acd7600da8f6cea574b1e484c0d2fcc6b
neisnei <neischalyruidiaz@gmail.com>
2023-06-26 16:04:28 +0000

    98battery

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <studio.h>
+
+/**
+ * pointer - function that takes a pointer the values points to 98.
+ *
+ * Return: Always 0
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
