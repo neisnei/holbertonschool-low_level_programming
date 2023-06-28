86edc440ef18e2727b332860026a9bb6d3fc5f65
neisnei <neischalyruidiaz@gmail.com>
2023-06-28 17:37:40 +0000

    concatenates

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: char
+ * @src: 0
+ * Return: 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int char *dest;
+	int char *src;
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+        dest[dest_len + i] = src[i];
+    dest[dest_len + i] = '\0';
+
+   return dest;
+}
