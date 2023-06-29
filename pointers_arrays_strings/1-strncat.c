5a5720fa7cb40d4ef7ac2c46976c6b867c186b42
neisnei <neischalyruidiaz@gmail.com>
2023-06-29 16:37:55 +0000

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: a pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b, c;
+
+	for (a= 0; dest[a] != '\0'; a++)
+		;
+
+	for (b = 0; src[b] != '\0'; b++)
+		;
+	for (c = 0; n[c] != '\0'; c++)
+	{
+		dest[a] = src[b] = n[c]
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
