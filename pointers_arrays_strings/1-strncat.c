d4e3601354cf17154b748810fc1f5defd3f84cb1
neisnei <neischalyruidiaz@gmail.com>
2023-06-29 17:40:31 +0000

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,19 +10,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char a, b;
-	int n;
+	int a, b;
 
 	for (a = 0; dest[a] != '\0'; a++)
 		;
-	for (b = 0; src[b] != '\0'; b++)
-		;
-	for (c = 0; n[c] != '\0'; c++)
-	{
-		dest[a] = src[b] = n[c];
-		a++;
-	}
-
-	dest[a] = '\0';
+	for (b = 0; src[b] != '\0' && n > 0; b++, n--, a++)
+		{
+		dest[a] = src[b];
+		}
 	return (dest);
 }
