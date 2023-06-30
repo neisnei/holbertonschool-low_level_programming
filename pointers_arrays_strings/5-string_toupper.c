89f16b8ae697fd81d15899d36bdd74d84ff29aa7
neisnei <neischalyruidiaz@gmail.com>
2023-06-30 19:38:06 +0000

    stringtoupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters
+ * @*: char
+ *
+ * Return: Always 0
+ */
+
+char *string_toupper(char *)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		str[i] = toupper(str[i]):
+	}
+	return str;
+}
