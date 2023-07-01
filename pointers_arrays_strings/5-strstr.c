693874ca4ed4ce0d5c200f6c0b4186360fb0dbf6
neisnei <neischalyruidiaz@gmail.com>
2023-07-01 02:05:45 +0000

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -4,21 +4,30 @@
  * _strstr - locates a substring
  * @haystack: char
  * @needle: char
- * Return: Always 0
+ * Return: Pointer to beginning of substring, or NULL if not found
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
+	int i, j, c;
+
+	i = 0;
+	c = 0;
+	while (haystack[i] != '\0')
 	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		j = 0;
+		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
+		       && needle[j + c] == haystack[i + c])
 		{
-			return (haystack);
+			if (haystack[i + c] != needle[j + c])
+				break;
+			c++;
 		}
-		else
-		{
-			haystack++;
-		}
+		if (needle[j + c] == '\0')
+			return (&haystack[i]);
+		j++;
+		i++;
 	}
-	return (0);
+
+	return (NULL);
 }
