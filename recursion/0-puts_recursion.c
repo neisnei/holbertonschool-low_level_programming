0f1aeef6b374561f6d32eed1889d36176791662c
neisnei <neischalyruidiaz@gmail.com>
2023-07-05 17:40:51 +0000

    putsrecursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints string
+ * _puts_recursion - print string
  * @s: char
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return;
+		_putchar('\n');
 
 	_putchar(*s);
 	_puts_recursion(s + 1);
