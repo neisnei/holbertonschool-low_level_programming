3e5defd385020db23ef2facb62c264d8fc09344b
neisnei <neischalyruidiaz@gmail.com>
2023-07-05 18:19:16 +0000

    strlenrecursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,9 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
-	while (*(s + count) != '\0')
-		count++;
-	return (count);
+	if (*s != '\0')
+	{
+		return (1 + (_strlen_recursion(s + 1)));
+	}
+	return (0);
 }
