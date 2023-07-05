f556c8aecf61753e010f77daca76745628a080d2
neisnei <neischalyruidiaz@gmail.com>
2023-07-05 15:18:08 +0000

    putsrecursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -9,9 +9,16 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar ('\n');
-		_putchar return;
+		putchar ('\n');
+	     return ;
 	}
-	_putchar (*s)
-	_puts_recursion(*s + 1);
+
+	putchar (*s);
+		return (_puts_recursion(s + 1));
+
+}
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
 }
