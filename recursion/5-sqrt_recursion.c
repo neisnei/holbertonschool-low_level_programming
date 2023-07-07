09d89b00171e864343b498f3da0ca695153080c1
neisnei <neischalyruidiaz@gmail.com>
2023-07-07 05:39:45 +0000

    sqrt

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of number
+ * @n: int
+ *
+ * Return: -1.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
