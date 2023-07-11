a5b6d802944e74c978a71b5f2ec737c15faa528f
neisnei <neischalyruidiaz@gmail.com>
2023-07-11 23:49:59 +0000

    malloccheck

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Function that allocates memory using malloc
+ * @b: Number to be checked
+ * Return: Pointer to allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
