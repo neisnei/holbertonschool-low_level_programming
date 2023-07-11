9128909e0062493fbfa2a6078cce107f132f6481
neisnei <neischalyruidiaz@gmail.com>
2023-07-11 22:24:27 +0000

    createarray

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,49 @@
+#include main.h
+
+/**
+ * create_array - creates an array of chars
+ * size: int
+ * c: int
+ * Return: a pointer to the array, or NULL
+ */
+
+char *create_array(unsigned int size, char c);
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *buffer;
+
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
+}
