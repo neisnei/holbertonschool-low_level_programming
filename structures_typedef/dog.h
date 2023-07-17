07d624bdcfadaf4a39357d4de559fcf9e92433ab
neisnei <neischalyruidiaz@gmail.com>
2023-07-17 15:40:14 +0000

    README.md

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,5 +14,17 @@ char *name;
 float age;
 char *owner;
 };
+
+int main (void)
+{
+  struct dog my_dog;
+
+  my_dog.name = "Reina"
+  my_dog.age = 3.5;
+  my_dog.owner = "Nei"
+
+  printf("Name: %s\n", my_dog.name);
+  printf("Age: %.1f\n", my_dog.age);
+  printf("Owner: %s\n", my_dog.age);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
