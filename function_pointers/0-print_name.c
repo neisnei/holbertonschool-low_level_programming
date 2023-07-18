9d604b2c1dedf2d5b33558ed3d7bdfe72604470c
neisnei <neischalyruidiaz@gmail.com>
2023-07-18 19:31:46 +0000

    README.md

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,10 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
  * print_name - Write a function that prints a name
  * @name: char
  * @f: char
- * 
  */
 
 void print_name(char *name, void (*f)(char *));
