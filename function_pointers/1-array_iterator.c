5354201af2a439d4268faf1b23728fc81eb3d6cb
neisnei <neischalyruidiaz@gmail.com>
2023-07-20 13:48:19 +0000

    README.md

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - write a function that executes a function given as a parameter
+ * array_iterator - write a function that executes a parameter
  * @array: int
  * @size: is the size of the array
  * @action: is a pointer
