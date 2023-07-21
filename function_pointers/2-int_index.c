bbf20eb61502b304e5001985c47f2851bc8641a6
neisnei <neischalyruidiaz@gmail.com>
2023-07-21 17:27:59 +0000

    README.md

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -3,10 +3,9 @@
 
 /**
  * int_index - Write a function searches a integer
- * @array:
- * @size:
- * @cmp:
- *
+ * @array: int
+ * @size: is the number of elements
+ * @cmp: is a pointer to used compare values
  * Return: 0 if false, something else otherwise
  */
 
@@ -18,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; ++i)
 		{
-			if(cmp(array[i]))
+			if (cmp(array[i]))
 				return (i);
 		}
 	}
