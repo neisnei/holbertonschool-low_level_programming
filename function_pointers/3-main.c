7b09f0375b1404a7649c000e0e58a95495354837
neisnei <neischalyruidiaz@gmail.com>
2023-07-21 17:24:06 +0000

    README.md

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3-calc.h"
+#include "calc.h"
 #include <stdio.h>
 
 /**
@@ -7,25 +7,3 @@
  * @argv: the argument vector
  * Return: Always 0
  */
-int main(int argc, char *argv[])
-{
-	int (*f)(int, int);
-
-	if (argc != 4)
-	{
-		puts("Error");
-		exit(98);
-	}
-
-	f = get_op_func(argv[2]);
-
-	if (!f)
-	{
-		puts("Error");
-		exit(99);
-	}
-
-	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
-
-	return (0);
-}
