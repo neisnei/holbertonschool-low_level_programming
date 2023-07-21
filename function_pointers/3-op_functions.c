57b23c3217f1000ed56ab55155a66394ea7dbf64
neisnei <neischalyruidiaz@gmail.com>
2023-07-21 17:10:16 +0000

    README.md

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,21 +1,60 @@
 #include "op_functions.c"
 
+/**
+ * op_add - add
+ * @a:first operand
+ * @b:second operand
+ * Return: the result of the operation
+ */
+
 int op_add (int a, int b)
 {
 	return (a+b);
 }
+
+/**
+ * op_sub - subtract
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
+
 int op_sub (int a, int b)
 {
 	return (a-b);
 }
+
+/**
+ * op_mul -multiply
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
+
 int op_mul (int a, int b)
 {
 	return (a*b);
 }
+
+/**
+ * op_div - divide
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
+
 int op_div (int a, int b)
 {
 	return (a/b);
 }
+
+/**
+ * op_mod - modulo
+ * @a: first operand
+ * @b: second operand
+ * Return: the result of the operation
+ */
+
 int op_mod (int a, int b)
 {
 	return (a%b);
