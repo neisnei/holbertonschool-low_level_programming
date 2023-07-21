b055e8ca1d53d9c9afcd78c8faf5970cd2115a92
neisnei <neischalyruidiaz@gmail.com>
2023-07-21 16:21:32 +0000

    README.md

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include 
+#include 
+
+/**
+ * get_op_func - select the correct function
+ * @s: the operation to perform
+ * Return: NULL
+ */
+
+int (*get_op_func(char *s))(int, int);
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+	i = 0;
+
+	while (i < s)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		++i;
+	}
+	return (NULL);
+}
