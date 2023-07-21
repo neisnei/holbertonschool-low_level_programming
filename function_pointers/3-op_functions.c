efbf9b3f1d5f712aa2609130ad76bfcd4ee595d6
neisnei <neischalyruidiaz@gmail.com>
2023-07-21 16:56:05 +0000

    README.md

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,22 @@
+#include "op_functions.c"
+
+int op_add (int a, int b)
+{
+	return (a+b);
+}
+int op_sub (int a, int b)
+{
+	return (a-b);
+}
+int op_mul (int a, int b)
+{
+	return (a*b);
+}
+int op_div (int a, int b)
+{
+	return (a/b);
+}
+int op_mod (int a, int b)
+{
+	return (a%b);
+}
