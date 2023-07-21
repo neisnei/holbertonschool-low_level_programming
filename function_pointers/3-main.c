9a730558acb34db0015be874fdcb327ec86900f1
neisnei <neischalyruidiaz@gmail.com>
2023-07-21 17:36:21 +0000

    README.md

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,9 +1,31 @@
-#include "calc.h"
-#include <stdio.h>
+#include "3-calc.h"
 
 /**
- * main - a basic calculator
- * @argc: the size of the argument vector
- * @argv: the argument vector
- * Return: Always 0
+ * main - This file should contain your main function only
+ * @argc: int
+ * @argv: array
+ * Return: always 0
  */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if (atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
