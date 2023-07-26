b48b71901d0554da34f2ed9ed3d3a6361b57fda4
neisnei <neischalyruidiaz@gmail.com>
2023-07-26 19:45:10 +0000

    README.md

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,13 +11,12 @@ int sum_them_all(const unsigned int n, ...)
 	int ret = 0;
 	unsigned int i;
 	va_list args;
-
-	if (n==0)
-		return (0);
+	if (n == 0)
+	return (0);
 
 	va_start(args, n);
 
-	for (i=0; i<n; i++)
+	for (i = 0; i < n; i++)
 		ret = ret + va_arg(args, int);
 	va_end(args);
 	return (ret);
