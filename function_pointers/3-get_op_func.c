3747b343f38544ffa94c9e1ff65ca4d06349a67d
neisnei <neischalyruidiaz@gmail.com>
2023-07-26 18:55:51 +0000

    README.md

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,30 +1,31 @@
 #include "3-calc.h"
 
 /**
- * get_op_func - select the correct function
- * @s: the operation to perform
- * Return: Return
+ * get_op_func -This file should contain the function
+ * @s: char
+ * Return: returns a pointer to the function
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
 	i = 0;
 
-	while (i < s)
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
-		++i;
+
+		i++;
 	}
 	return (0);
 }
