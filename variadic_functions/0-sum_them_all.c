03a9f63763cdf35b2467892277b3abf4d6be44d5
neisnei <neischalyruidiaz@gmail.com>
2023-07-26 19:57:50 +0000

    README.md

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,23 +1,21 @@
 #include "variadic_functions.h"
 /**
- * sum_them_all - sums up all the function arguments
+ * sum_them_all - sums all arguments passed
+ *
  * @n: number of arguments
  *
- * Return: the sum or 0
+ * Return: sum of arguments
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int ret = 0;
 	unsigned int i;
-	va_list args;
+	int sum;
+	va_list list;
 
-	if (n == 0)
-		return (0);
+	va_start(list, n);
+	for (i = 0, sum = 0; i < n; i++)
+		sum += va_arg(list, int);
 
-	va_start(args, n);
-
-	for (i = 0; i < n; i++)
-		ret = ret + va_arg(args, int);
-	va_end(args);
-	return (ret);
+	va_end(list);
+	return (sum);
 }
