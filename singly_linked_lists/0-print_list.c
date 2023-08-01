e563273346597f3c5eb715e79bcdf7899a4ee7aa
neisnei <neischalyruidiaz@gmail.com>
2023-08-01 18:59:40 +0000

    README.md

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ *
+ * Return: Always 0.
+ */
+
+size_t print_list(const list_t *h)
+{
+
