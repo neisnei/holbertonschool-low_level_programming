97a06ebbda565f7ab8f2c5fcd1d119516e33196f
neisnei <neischalyruidiaz@gmail.com>
2023-08-01 20:35:50 +0000

    README.md

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -5,10 +5,21 @@
 
 /**
  * print_list - prints all the elements of a list
- *
+ *@h: head of the list
  * Return: Always 0.
  */
 
 size_t print_list(const list_t *h)
 {
+	int num;
 
+	for (num = 0; h != NULL; num++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (num);
+}
