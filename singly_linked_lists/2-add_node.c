898901d4a43fb7c55cce85d5b8311b2f95c26a8a
neisnei <neischalyruidiaz@gmail.com>
2023-08-02 15:53:08 +0000

    README.md

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,24 +1,23 @@
 #include "lists.h"
-
 /**
- *add_node_end - adds a new node at the end
- *@head:needs to be duplicated
- *@str:string
- *Return: the address of the new element, or NUL
+ * add_node - functions that adds a new mode at the beginning of list
+ * @head: pointing to the beginning of the string
+ * @str: pointer to the string
+ * Return: the address of the new elements, or NULL if it failed
  */
-
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *adding;
 
 	adding = malloc(sizeof(list_t));
 
-	if (adding == NULL) || str == (NULL)
+	if (adding == NULL || str == NULL)
 		return (NULL);
 
 	adding->str = strdup(str);
 	adding->len = strlen(str);
 	adding->next = *head;
 	*head = adding;
+
 	return (adding);
 }
