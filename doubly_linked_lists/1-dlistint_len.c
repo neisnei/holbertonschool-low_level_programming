ccde9406b9dbf3e56feb857b2304e43806cc2bf3
neisnei <neischalyruidiaz@gmail.com>
2023-08-04 20:41:38 +0000

    README.md

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 
 /**
- * dlistint_len - returns the number of elements in a linked list
+ * dlistint_len - returns the number of elements
  * @h: head
- * @return: Always EXIT_SUCCESS
+ * @return: Always 0
  */
 
 size_t dlistint_len(const dlistint_t *h)
