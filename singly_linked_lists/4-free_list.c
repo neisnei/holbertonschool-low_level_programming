bab9449901c6dc89d14a8fc9f553a6251679fa27
neisnei <neischalyruidiaz@gmail.com>
2023-08-04 19:31:17 +0000

    README.md

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head
+ * Return: Void
+*/
+
+void free_list(list_t *head)
+{
+    list_t *current = head;
+
+    while (current != NULL)
+    {
+        list_t *next = current->next;
+        free(current->str);
+        free(current);
+        current = next;
+    }
+}
