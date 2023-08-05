5d8acbf3d511f2256eea99b91026f857bba4eedc
neisnei <neischalyruidiaz@gmail.com>
2023-08-05 14:44:11 +0000

    README.md

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,7 +4,28 @@
 #include "lists.h"
 
 /**
- * main - check the code
- *
+ * add_dnodeint - adds new node
+ * @head: head of the list
+ * @n: data be allocated
  * Return: Always 0
  */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+
+	if (*head != NULL)
+		(*head)->prev = new_node;
+
+	*head = new_node;
+	return (new_node);
+}
