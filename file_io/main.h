991305868cfba6fa07e5b7835610e910b51b58a9
neisnei <neischalyruidiaz@gmail.com>
2023-08-10 21:48:10 +0000

    README.md

--- file_io/main.h
+++ file_io/main.h
@@ -1,21 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
-/**
- * 
- * @
- * @
- * @
- *
- * Description: 
- * 
- */
-
-typedef struct
-
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 cp file_from file_to
+
+#endif
