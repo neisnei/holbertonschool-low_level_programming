ac212b5337ca038b330bb3ea108f12da438e3053
neisnei <neischalyruidiaz@gmail.com>
2023-08-11 03:34:21 +0000

    README.md

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: Always EXIT_SUCCESS
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	hash_node_t **array; 
+	unsigned long int i; 
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL); 
+
+	array = malloc(sizeof(hash_node_t *) * size);
+
+	if (array == NULL) 
+    {
+		free(hash_table); 
+		return (NULL); 
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	hash_table->size = size;
+	hash_table->array = array;
+
+	return (hash_table); 
+}
