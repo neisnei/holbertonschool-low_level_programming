3098c250732dcaa79051ad6022cf95c52ec19fee
neisnei <neischalyruidiaz@gmail.com>
2023-08-11 04:12:48 +0000

    README.md

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
-include "hash_tables.h"
+#include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
+ * hash_table_create - creates a pointer filled hash table
  *
  * @size: size of the array
  * Return: the newly created hash table
